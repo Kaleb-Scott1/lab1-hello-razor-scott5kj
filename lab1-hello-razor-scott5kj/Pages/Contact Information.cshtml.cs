8c5e135662e01210935ad696a90acf18d5625071
Kaleb-Scott1 <kalebjscott24@gmail.com>
2025-09-06 16:48:38 -0400

    Add project files.

---
+++ lab1-hello-razor-scott5kj/Pages/Contact Information.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab1_hello_razor_scott5kj.Pages
+{
+    public class Contact_InformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
